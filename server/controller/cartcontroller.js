45b543f7679c44fa72d8ec588171e1f4e0f5eb0b
Ahmad Jamil Alfakori <ahmadjkff1@gmail.com>
2025-09-27 09:19:23 +0300

    Implement cart functionality: Enable adding, retrieving, Update routes and controller for improved user experience and error handling.

--- server/controller/cartcontroller.js
+++ server/controller/cartcontroller.js
@@ -1,78 +1,88 @@
-const cart=require("../models/cart")
+const cart = require("../models/cart");
+const userModel = require("../models/user");
 
-exports.addToCart=async(req,res)=>{
-    const {userId,productId,quantity}=req.body;
-    try {
-        let userCart=await cart.findOne({userId});
-        if(userCart){
-            const productIndex=userCart.products.findIndex(p=>p.productId.toString()===productId);
-            if(productIndex>-1){
-                userCart.products[productIndex].quantity+=quantity;
-            }
-            else{
-                userCart.products.push({productId,quantity});
-            }   
-            await userCart.save();
-            res.status(200).json(userCart);
-        }  
-        else{
-            const newCart=await cart.create({
-                userId,
-                products:[{productId,quantity}]
-            });
-            res.status(200).json(newCart);
-        }
-    } catch (error) {
-        res.status(400).json({message:error.message})
-    } 
-    
-}
-exports.getCart=async(req,res)=>{
-    const userId=req.params.userId; 
-    try {
-        const userCart=await cart.findOne({userId}).populate("products.productId"); 
-        if(!userCart){
-            return res.status(404).json({message:"cart not found"})
-        }
-        res.status(200).json(userCart);
-    } catch (error) {
-        res.status(400).json({message:error.message})
-    }   
-}
-
-exports.removeFromCart=async(req,res)=>{
-    const {userId,productId}=req.body;  
-    try {
-        const userCart=await cart.findOne({userId
-});
-        if(!userCart){
-            return res.status(404).json({message:"cart not found"})
-        }   
-        const productIndex=userCart.products.findIndex(p=>p.productId.toString()===productId);
-        if(productIndex>-1){
-            userCart.products.splice(productIndex,1);
-            await userCart.save();
-            return res.status(200).json(userCart);
-        }
-        else{
-            return res.status(404).json({message:"product not found in cart"})
-        }
-    } catch (error) {
-        res.status(400).json({message:error.message})
+exports.addToCart = async (req, res) => {
+  const userId = req.params.userId;
+  const { productId, quantity } = req.body;
+  try {
+    let userCart = await cart.findOne({ userId });
+    if (userCart) {
+      const productIndex = userCart.products.findIndex(
+        (p) => p.productId.toString() === productId
+      );
+      if (productIndex > -1) {
+        userCart.products[productIndex].quantity += quantity;
+      } else {
+        userCart.products.push({ productId, quantity });
+      }
+      await userCart.save();
+      res.status(200).json(userCart);
+    } else {
+      const newCart = await cart.create({
+        userId,
+        products: [{ productId, quantity }],
+      });
+      res.status(200).json(newCart);
     }
-}
-exports.clearCart=async(req,res)=>{
-    const userId=req.params.userId;
-    try {
-        const userCart=await cart.findOne({userId});
-        if(!userCart){
-            return res.status(404).json({message:"cart not found"})
-        }           
-        userCart.products=[];
-        await userCart.save();
-        res.status(200).json(userCart);
-    } catch (error) {
-        res.status(400).json({message:error.message})
-    }   
-}
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
+exports.getCart = async (req, res) => {
+  const userId = req.params.userId;
+  try {
+    const user = await userModel.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "user not found" });
+    }
+
+    const userCart = await cart
+      .findOne({ userId })
+      .populate("products.productId");
+    if (!userCart) {
+      return res.status(404).json({ message: "cart not found" });
+    }
+
+    res.status(200).json(userCart);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.removeFromCart = async (req, res) => {
+  const { userId, productId } = req.body;
+  try {
+    const userCart = await cart.findOne({ userId });
+    if (!userCart) {
+      return res.status(404).json({ message: "cart not found" });
+    }
+    const productIndex = userCart.products.findIndex(
+      (p) => p.productId.toString() === productId
+    );
+    if (productIndex > -1) {
+      userCart.products.splice(productIndex, 1);
+      await userCart.save();
+      return res.status(200).json(userCart);
+    } else {
+      return res.status(404).json({ message: "product not found in cart" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.clearCart = async (req, res) => {
+  const userId = req.params.userId;
+  try {
+    const userCart = await cart.findOne({ userId });
+    if (!userCart) {
+      return res.status(404).json({ message: "cart not found" });
+    }
+    userCart.products = [];
+    await userCart.save();
+    res.status(200).json(userCart);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
--- server/routes/cartRoutes.js
+++ server/routes/cartRoutes.js
@@ -1,1 +1,10 @@
+const express = require("express");
+const { getCart, addToCart } = require("../controller/cartcontroller");
 
+const routes = express.Router();
+require("dotenv").config();
+
+routes.get("/:userId", getCart);
+routes.post("/:userId/add", addToCart);
+
+module.exports = routes;
