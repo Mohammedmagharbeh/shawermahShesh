e788151962cb6422de99cc6dd72fab4dfa4b1848
Ahmad Jamil Alfakori <ahmadjkff1@gmail.com>
2025-09-28 01:23:57 +0300

    Add locations route and update cart controller: Refactor cart methods for improved readability and error handling, and modify order model to reference locations.

--- server/routes/cartRoutes.js
+++ server/routes/cartRoutes.js
@@ -1,20 +1,20 @@
-const { getCart, addToCart } = require("../controller/cartcontroller");
-
 const express = require("express");
 const cors = require("cors");
 const routes = express.Router();
 require("dotenv").config();
 
-
-const{removeFromCart,updatecart,clearCart}=require("../controller/cartcontroller")
-
-
-routes.put("/updatecart/:id", updatecart);
-routes.delete("/deletecart", removeFromCart);
-routes.delete("/clearcart/:userId", clearCart);
-require("dotenv").config();
+const {
+  removeFromCart,
+  updatecart,
+  clearCart,
+  getCart,
+  addToCart,
+} = require("../controller/cartcontroller");
 
 routes.get("/:userId", getCart);
 routes.post("/:userId/add", addToCart);
+routes.put("/updatecart/:id", updatecart);
+routes.delete("/deletecart", removeFromCart);
+routes.delete("/clearcart/:userId", clearCart);
 
 module.exports = routes;
