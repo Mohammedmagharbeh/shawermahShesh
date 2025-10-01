220452ded0d00bb887c4936c3f371ae7110be712
Ahmad Jamil Alfakori <ahmadjkff1@gmail.com>
2025-10-01 20:23:17 +0300

    Update AdminDashboard: Change socket connection URL to use the production server for real-time order notifications.

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import newOrderSound from "../../public/newOrder.mp3";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://shawermahshesh-1.onrender.com");
 
 function AdminDashboard() {
   const [orders, setOrders] = useState([]);
