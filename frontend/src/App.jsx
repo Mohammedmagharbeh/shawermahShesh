bd923225c4e567d2f788a9823f95df4b39db056b
Ahmad Jamil Alfakori <ahmadjkff1@gmail.com>
2025-10-31 23:09:20 +0300

    Update import path for AdminProductPanel to include file extension for clarity

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,7 @@ import AdminUsersPage from "./pages/adminusers";
 import Products from "./pages/Products";
 import { useUser } from "./contexts/UserContext";
 import AdminSlides from "./pages/AdminSlides";
-import AdminProductPanel from "./pages/menuManagement/AdminProductPanel";
+import AdminProductPanel from "./pages/menuManagement/AdminProductPanel.jsx";
 
 function App() {
   const { i18n } = useTranslation();
