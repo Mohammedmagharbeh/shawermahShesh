641f38bd710dc0df669e27f8f5c9a8c921bb0984
Mohammedmagharbeh <magharbehmohammed@gmail.com>
2026-06-30 16:42:59 +0300

    fix: adjust logo size for improved responsiveness in Header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -384,7 +384,7 @@ function Header() {
             <div className="flex items-center gap-1.5 xs:gap-2 sm:gap-3">
               {/* الشعار الأول (أكبر بشكل ملحوظ + Responsive ممتاز) */}
               {/* <div className="w-16 h-16 xs:w-20 xs:h-20 sm:w-24 sm:h-24 md:w-28 md:h-20 rounded-full"> */}
-              <div className="w-16 h-16 xs:w-20 xs:h-20 sm:w-24 sm:h-24 md:w-28 md:h-20 lg:w-32 lg:h-24 xl:w-36 xl:h-28 rounded-full">
+              <div className="w-20 h-20 xs:w-20 xs:h-20 sm:w-24 sm:h-24 md:w-28 md:h-20 lg:w-32 lg:h-24 xl:w-36 xl:h-28 rounded-full">
 
               {/* <div className="w-20 h-20 xs:w-24 xs:h-24 sm:w-28 sm:h-28 md:w-32 md:h-32"> */}
                 <img
